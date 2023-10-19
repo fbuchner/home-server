ac51a84a794330e7170c8e6be5e3b14bb4045189
Frederic Buchner <25030946+fbuchner@users.noreply.github.com>
2023-10-19 21:26:55 +0200

    Update navidrome-create-playlists.sh

--- scripts/navidrome-create-playlists.sh
+++ scripts/navidrome-create-playlists.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script iterates over all subfolders in the given path 
+# and creates an m3u-playlist with the mp3s for each subfolder 
+
 for d in /usr/media/music/*/; do
    echo "$d"
    ls "$d"/*.mp3 > "$d/$(basename "$d").m3u"
